7108da64480031952f9d5a89dd01f9eafc63f69d
SirAndreRusso <102429266+SirAndreRusso@users.noreply.github.com>
2022-04-23 02:11:28 +0300

    добавлен таймер, кнопка для запуска новой игры, доработана логика и добавлены комментарии. Требуется доработка autolayout

--- Find Numbers/Game.swift
+++ Find Numbers/Game.swift
@@ -6,42 +6,113 @@
 //
 
 import Foundation
+// Enumeration of game statuses
+// Состояния игры
 enum StatusGame {
     case start
     case win
+    case lose
 }
+// A class that determines the model of the game
+// Класс,описывающий модель игры
 class Game {
+    // A struct that determines an item (button)
+    // Структура, описывающая айтем (кнопку)
     struct Item {
         var title: String
         var isFound: Bool = false
+        var isError = false
     }
-    
+    // An array of numbers containing the data to use in array of items
+    // Массив чисел, из которого будем брать элементы для массива айтемов
     private let data = Array(1...99)
+    // empty array for items
     var items: [Item] = []
     private var countItems: Int
+    // Число, передаваемое в лейбл, которое нужно найти среди кнопок с числами
+    // The next number in lable
     var nextItem: Item?
-    var status: StatusGame = .start
-    init(countItems: Int) {
-        self.countItems = countItems
-        setupGame () 
+    // Default status of the game with observer
+    // Значение статуса по умолчанию, с наблюдателем
+    var status: StatusGame = .start {
+        didSet{
+            if status != .start{
+                stopGame()
+            }
+        }
     }
+    //Defining time for game
+    // Определяем игровое время
+    private var timeForGame: Int
+    // Time left
+    // Оставшееся для игры время
+    private var secondsGame: Int {
+        didSet {
+            if secondsGame == 0{
+                status = .lose
+            }
+            updateTimer(status, secondsGame)
+        }
+    }
+    private var timer: Timer?
+    // ??
+    private var updateTimer:((StatusGame, Int)->Void)
+    init(countItems: Int, time: Int, updateTimer: @escaping (_ status: StatusGame, _ seconds: Int) -> Void) {
+        self.countItems = countItems
+        self.timeForGame = time
+        self .secondsGame = time
+        self.updateTimer = updateTimer
+        setupGame()
+    }
+    // создание необходимого количества уникальных айтемов и числа, которое необходимо найти, установка таймера
+    // Preparing the game by creating uniq items and adding items in array. Creating next number to find. timer setup
     private func setupGame(){
         var digits = data.shuffled()
+        items.removeAll()
         while items.count < countItems {
-            var item = Item(title: String(digits.removeFirst()))
+            let item = Item(title: String(digits.removeFirst()))
             items.append(item)
         }
         nextItem = items.shuffled().first
+        updateTimer(status, secondsGame)
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {[weak self] (_) in
+            self?.secondsGame -= 1
+        })
     }
+    
+    func newGame(){
+        status = .start
+        self.secondsGame = self.timeForGame
+        setupGame()
+        
+    }
+    // Checking up numbers to
+    // проверка числа на соответствие числу из лейбла, которое нужно найти. когда все числа будут найдены, сменить статус на .win
     func check (index: Int) {
+        guard status == .start else {return}
         if items[index].title == nextItem?.title {
             items[index].isFound = true
             nextItem = items.shuffled().first(where: {(Item)-> Bool in Item.isFound == false})
-            
+        }   else {
+            items[index].isError = true
         }
         if nextItem == nil {
             status = .win
         }
     }
+    // Stopping the timer
     
+    private func stopGame () {
+        timer?.invalidate()
+    }
+    
+}
+// Making timerlabel info more comfortable
+// Делаем удобное представление для timerLabel
+extension Int{
+    func secondsToString()->String{
+        let minutes = self / 60
+        let seconds = self % 60
+        return String(format: "%d:%02d", minutes, seconds)
+    }
 }
--- Find Numbers/GameViewController.swift
+++ Find Numbers/GameViewController.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    @IBOutlet weak var timerLabel: UILabel!
+    
     @IBOutlet weak var statusLabel: UILabel!
-    @IBOutlet weak var nextDigit: UILabel!
+    @IBOutlet weak var nextDigitLabel: UILabel!
     @IBOutlet var buttons: [UIButton]!
-    lazy var game = Game(countItems:buttons.count)
+    @IBOutlet weak var newGameButton: UIButton!
+    // Creating Game object after buttons outlets will be loaded in viewdidload
+    // Создается экземпляр класса Game после загрузки аутлетов кнопок
+    lazy var game = Game(countItems: buttons.count, time: 30) { [weak self] (status, time) in
+        guard let self = self else {return}
+        self.timerLabel.text = time.secondsToString()
+        self.updateInfoGame(with: status)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupScreen ()
@@ -22,24 +31,59 @@ class GameViewController: UIViewController {
         guard let buttonIndex = buttons.firstIndex(of: sender) else {return}
         game.check(index: buttonIndex)
         updateUI()
-
+        
     }
+    // Setting up buttons titles in appliance with items titles. Setting up label with next number. Making all buttons visible
+    // кнопкам на экране присваиваются тайтлы, соответствующие числам из массива айтемов. В лейбл добавляется число, которое нужно найти. Все кнопки становятся видимыми
+    
     private func setupScreen () {
         for index in game.items.indices {
             buttons[index].setTitle(game.items[index].title, for: .normal)
-            buttons[index].isHidden = false
+            buttons[index].alpha = 1
+            buttons[index].isEnabled = true
         }
-        nextDigit.text = game.nextItem?.title
+        nextDigitLabel.text = game.nextItem?.title
+        
     }
+    // Hiding the buttons after they  was found. Changing the label text after game status updated to .win
+    // Скрываем кнопки после того как найдены искомые числа. Обновляем лейбл после победы
     private func updateUI() {
         for index in game.items.indices {
-            buttons[index].isHidden = game.items[index].isFound
+            buttons[index].alpha = game.items[index].isFound ? 0 : 1
+            buttons[index].isEnabled = !game.items[index].isFound
+            if game.items[index].isError {
+                UIView.animate(withDuration: 0.3) {[weak self]  in
+                    self?.buttons[index].backgroundColor = .red
+                } completion: {[weak self] (_) in
+                    self?.buttons[index].backgroundColor = .white
+                    self?.game.items[index].isError = false
+                }
+
+            }
         }
-        nextDigit.text = game.nextItem?.title
-        if game.status == .win {
-            statusLabel.text = "вы выиграли!"
-            statusLabel.textColor = .green
-        }
+        nextDigitLabel.text = game.nextItem?.title
+        updateInfoGame(with: game.status)
+}
+private func updateInfoGame(with status: StatusGame) {
+    switch status {
+    case .start:
+        statusLabel.text = "Игра началась"
+        statusLabel.textColor = .black
+        newGameButton.isHidden = true
+    case .win:
+        statusLabel.text = "Вы выиграли!"
+        statusLabel.textColor = .green
+        newGameButton.isHidden = false
+    case .lose:
+        statusLabel.text = "Вы проиграли"
+        statusLabel.textColor = .red
+        newGameButton.isHidden = false
+    }
+}
+    @IBAction func newGame(_ sender: UIButton) {
+        game.newGame()
+        sender.isHidden = true
+        setupScreen()
     }
 }
 
